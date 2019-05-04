b648fb9fc2b61facb0015bc601eae523a199ff17
HiranD <hiranhsw@gmail.com>
2019-05-04 10:21:46 +0530

    optional functionalities

--- README.md
+++ README.md
@@ -2,21 +2,29 @@
 
 	unbabel_cli --input_file events.json --window_size 10
 
+I have selected pandas to process data because of it's easyness to change, in case if I want to add more features.
+
 ### _*python solution.py --help*_
 
 ```
 optional arguments:
-    --output_loc      Relative path to save results
-    --to              Relative time (yyyy-mm-dd_HH:MM) to start calculation
+    --output_loc      Relative path to save results (Defult: same directory output_result.json).
+    --now             Relative time (yyyy-mm-dd_HH:MM) to start calculation (Defult: now()).
 ```
-*eg: python solution.py --input_file events.json --window_size 10 --to 2019-01-02_00:00 --output_loc ../output/result.json*
+*eg: python solution.py --input_file events.json --window_size 10 --now 2019-01-02_00:00 --output_loc ../output/result.json*
 
 ### Notes
-this events.json contains artificially generated translation delivery json logs within the day of 2019-01-01. So if you are using this json file you will have to give a time within day of 2019-01-01 as *--to* argument. So that will become the ancore point and amount of *window_size* minutes will go back from that point backwards.
+This events.json (generated using [json-generator.com](https://www.json-generator.com)) contains artificially generated translation delivery json logs within the day of 2019-01-01. So if you are using this json file you will have to give a time within day of 2019-01-01 as *--now* argument. So that will become the ancore point and amount of __window_size__ minutes will go back from that point backwards.
 
-*eg: in the above example calcualtion will only consider the data from 2019-01-01 23:50 to 2019-01-02 00:00 only*
+*eg: in the above example, calcualtion will only consider the data from 2019-01-01 23:50 to 2019-01-02 00:00 only.*
 
-If we are not defining attribute *--to*, calculation will start from _now_ on backwards.
+If we are not defining attribute *--now*, calculation will start from __now__ on backwards.
 
 ### Additional features
+    --translation       Filter results by translation type (source_language:target_language).
 
+*eg: python solution.py --input_file events.json --window_size 10 --now 2019-01-02_00:00 --translation en:fr*
+
+### Requirements
+    python 3.6
+    pandas 0.23.4
--- solution.py
+++ solution.py
@@ -1,91 +1,159 @@
-import argparse
-from datetime import datetime, timedelta
+import sys
 import json
+import argparse
+import traceback
 import pandas as pd
+from datetime import datetime, timedelta
 
-def main(args):   
+
+def main(args):
     # Calculating start and end time for the processing
     if args.now_ == "now":
-        now_ = datetime.now().replace(microsecond=0,second=0)-timedelta(minutes=1)
-        # now_ = datetime(2019, 1, 2)
+        now_ = datetime.now().replace(microsecond=0, second=0)-timedelta(minutes=1)
     else:
         try:
             now_ = datetime.strptime(args.now_, "%Y-%m-%d_%H:%M")
         except ValueError:
             print("Given date-time is not comply with needed format -> yyyy-mm-dd_HH:MM")
+            sys.exit(1)
+        except:
+            print("Error: Check following stack trace for more info.")
+            print(traceback.format_exc())
+            sys.exit(1)
 
     from_ = now_ - timedelta(minutes=args.window_size)
-    print("Started calculation from " + str(from_) + " to " + str(now_))
+    print("Started calculation from {} to {}.".format(str(from_), str(now_)))
+
     # Read json bojects to a list
     json_list = []
-    with open(args.path_to_json) as f:
-        for line in f:
-            json_list.append(json.loads(line))
+    try:
+        with open(args.path_to_json) as f:
+            for line in f:
+                json_list.append(json.loads(line))
+    except ValueError as e:
+        print("Please check the given file content comply with needed JSON format -> " + str(e))
+        sys.exit(1)
+    except:
+        print("Error: Check following stack trace for more info.")
+        print(traceback.format_exc())
+        sys.exit(1)
+
     # Selects record within our time-gap
     data = []
-    for record in json_list:
-        t = datetime.strptime(record['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
-        if (from_ <= t and now_ > t):
-            data.append(record)
+    try:
+        for record in json_list:
+            t = datetime.strptime(record['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+            if (from_ <= t and now_ > t):
+                data.append(record)
+    except ValueError as e:
+        print("Error in timestamp conversion -> " + str(e))
+        sys.exit(1)
+    except:
+        print("Error: Check following stack trace for more info.")
+        print(traceback.format_exc())
+        sys.exit(1)
+
+    checkDataAmount(data)
+
     # In case sorting the rocords by time
-    sortedData = sorted(data,key=lambda x: datetime.strptime(x['timestamp'], '%Y-%m-%d %H:%M:%S.%f'), reverse=False)
+    sortedData = sorted(data, key=lambda x: datetime.strptime(
+        x['timestamp'], '%Y-%m-%d %H:%M:%S.%f'), reverse=False)
     # Creating a dataframe from selected json objets
-    df = pd.DataFrame(sortedData, columns=['timestamp','duration','nr_words','source_language','target_language','client_name'])
+    df = pd.DataFrame(sortedData, columns=[
+                      'timestamp', 'duration', 'nr_words', 'source_language', 'target_language', 'client_name'])
+
+    # Translation filter
+    if args.translate is not None:
+        try:
+            [source_language, target_language] = args.translate.split(':')
+        except:
+            print("Please check --translation parameter has defined correctly.")
+        else:
+            if target_language not in df.target_language.unique():
+                print('Given target_language does not exist!')
+                sys.exit(1)
+            elif source_language not in df.source_language.unique():
+                print('Given source_language does not exist!')
+                sys.exit(1)
+            else:
+                print("Filter results by translation -> source_language:{}, target_language:{}".format(
+                    source_language, target_language))
+                df = df[df.target_language == target_language]
+                df = df[df.source_language == source_language]
+                checkDataAmount(df.index)
+
     # Conveting to datetime and making index out of timestamp
     df['timestamp'] = pd.to_datetime(df.timestamp)
     df = df.set_index('timestamp')
     # Calculating moving average
     df['MA'] = df[['duration']].rolling('1min').mean().round(2)
-    # Removing other columns, reseting index and rounding time to ceiling minute 
+    # Removing other columns, reseting index and rounding time to ceiling minute
     df = df[['MA']]
     df = df.reset_index()
     df['timestamp'] = df['timestamp'].dt.ceil('min')
     # First minute result shoud be removed, because it was partially calcualted
     df = df.iloc[1:]
-    # Remove duplicate rows using timestamp and keep last 
-    df = df.drop_duplicates(subset='timestamp', keep='last').reset_index(drop=True)
+    # Remove duplicate rows using timestamp and keep last
+    df = df.drop_duplicates(
+        subset='timestamp', keep='last').reset_index(drop=True)
     # Renaming and converting to string
-    df.rename(columns={'timestamp': 'time', 'MA': 'average_delivery_time'}, inplace=True)
+    df.rename(columns={'timestamp': 'time',
+                       'MA': 'average_delivery_time'}, inplace=True)
     df['time'] = df['time'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
     # Finally writing to the output file in required format
     result_dict = df.to_dict(orient='records')
     try:
         with open(args.path_to_output, "w") as write_file:
             for object_ in result_dict:
                 write_file.write(json.dumps(object_) + '\n')
-    except:
-        print("There is an error when saving the results!!")
+    except Exception as e:
+        print("There is an error when saving the results -> " + str(e))
     else:
         print("Results successfully saved at " + args.path_to_output)
 
 
-# Parsing arguments from command prompt   
+def checkDataAmount(data):
+    # Checking for the amount of data after filtering.
+    if len(data) == 0:
+        print("No data within the specified time.")
+        sys.exit(1)
+    # I just use window_size. Because its seems like a better choice than putting a constant value.
+    elif len(data) <= args.window_size:
+        print("Warning: Amount of data within the specified time is very less.")
+
+
+# Parsing arguments from command prompt
 parser = argparse.ArgumentParser()
 parser.add_argument('--input_file',
                     dest='path_to_json',
                     required=True,
-                    type = str,
-                    help='Relative path to log file')
+                    type=str,
+                    help='Relative path to log file.')
 
 parser.add_argument('--window_size',
                     dest='window_size',
-                    default=False,
                     required=True,
-                    type = int,
-                    help='Size of time gap in minutes')
+                    type=int,
+                    help='Size of time gap in minutes.')
 
 parser.add_argument('--output_loc',
                     dest='path_to_output',
                     default='output_result.json',
-                    type = str,
-                    help='Relative path to save results')
+                    type=str,
+                    help='Relative path to save results.')
 
-parser.add_argument('--to',
+parser.add_argument('--now',
                     dest='now_',
                     default='now',
-                    type = str,
-                    help='Relative time (yyyy-mm-dd_HH:MM) to start calculation')
+                    type=str,
+                    help='Relative time (yyyy-mm-dd_HH:MM) to start calculation.')
 
-if __name__== "__main__":
+parser.add_argument('--translation',
+                    dest='translate',
+                    type=str,
+                    help='Filter results by translation type (source_language:target_language)')
+
+if __name__ == "__main__":
     args = parser.parse_args()
     main(args)
